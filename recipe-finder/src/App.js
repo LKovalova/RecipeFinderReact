8f60eb2c68589df1ecd4f96107a141732bbb1f30
Liubov Kovalova <liuba532013@gmail.com>
2024-09-25 23:10:11 -0300

    Added initial Search component

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <Header />
+      <Search />
     </div>
   );
 }
---
+++ recipe-finder/src/components/Search.js
@@ -0,0 +1,12 @@
+// src/components/Search.js
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Recipe Finder</h1>
+    </div>
+  );
+};
+
+export default Search;
