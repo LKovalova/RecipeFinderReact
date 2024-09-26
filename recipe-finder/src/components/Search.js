56a6009819a11e10a76e32a2049273b88e6b8a69
Liubov Kovalova <liuba532013@gmail.com>
2024-09-25 23:13:02 -0300

    Added search input

--- recipe-finder/src/components/Search.js
+++ recipe-finder/src/components/Search.js
@@ -1,12 +1,21 @@
-// src/components/Search.js
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+  const [cuisine, setCuisine] = useState("");
+
   return (
     <div>
       <h1>Recipe Finder</h1>
+      <input 
+        type="text" 
+        placeholder="Search for recipes..." 
+        value={query} 
+        onChange={(e) => setQuery(e.target.value)} 
+      />
     </div>
   );
 };
 
 export default Search;
+
