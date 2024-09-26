059bbd60ef4c1a2226844bd4321fed4aa034f322
Liubov Kovalova <liuba532013@gmail.com>
2024-09-25 23:40:51 -0300

    Implemented recipe fetching in App component

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -1,14 +1,32 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fetchRecipes } from "./api";
 import "./App.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [searchPerformed, setSearchPerformed] = useState(false);
+
+  const handleSearch = async (query, cuisine) => {
+    const data = await fetchRecipes(query, cuisine, 1, 5);
+    setRecipes(data.results);
+    setSearchPerformed(true);
+  };
+
   return (
-    <div>
-      <Header />
-      <Search />
-    </div>
+    <Router>
+      <Search onSearch={handleSearch} />
+      <div>
+        {searchPerformed ? (
+          recipes.map((recipe) => <div key={recipe.id}>{recipe.title}</div>)
+        ) : (
+          <p>Please perform a search to display results.</p>
+        )}
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
